a34ade580916354329c435d8795da0ec9ec0dafb
yasmine.morawej@gmail.com <yasmine.morawej@gmail.com>
2019-12-02 20:10:09 +0000

    add todo component & pass state via a prop

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Todos from './components/Todos';
 
-function App() {
-  return (
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Wash up',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Have a bath',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Drink more wine',
+        completed: false
+      }
+    ]
+  }
+  render() {
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Todos todos={this.state.todos}/>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
