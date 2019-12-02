b0c0a7bcc4d8476010c08977f4dbfc2e5b7da684
yasmine.morawej@gmail.com <yasmine.morawej@gmail.com>
2019-12-02 20:39:49 +0000

    add key onto TodoItem list

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -4,9 +4,10 @@ import TodoItem from './TodoItem' ;
 class Todos extends Component {
   render() {
     return this.props.todos.map((todo) => (
-    <TodoItem />
+    <TodoItem key={todo.id} todo={todo} />
     ));
   }
 }
 
 export default Todos;
+
