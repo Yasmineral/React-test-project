f5531aba231cff9a894a861fc0c11b90cdd5dcdd
yasmine.morawej@gmail.com <yasmine.morawej@gmail.com>
2019-12-05 14:50:59 +0000

    add input form for todo items & css styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Header from './components/layout/Header';
 import './App.css';
 import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
 
 class App extends Component {
   state = {
@@ -44,9 +45,12 @@ class App extends Component {
   render() {
     return ( 
     <div className="App">
+      <div className="container">
       <Header />
+      <AddTodo />
       <Todos todos={this.state.todos} markComplete={this.markComplete}
       delTodo={this.delTodo} />
+      </div>
     </div>
     );
   }
